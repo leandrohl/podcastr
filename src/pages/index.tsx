f37829692e8a7f93ec504757cf6457ae52ca29ff
Leandro Henrique <leandro.henrique@unesp.br>
2021-04-21 16:40:21 -0300

    implementando o SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,27 @@
 
+// SPA
+// SSR
+// SSG
 
-export default function Home() {
+import { useEffect } from "react"
+
+export default function Home(props) {
+
   return (
     <div >
       <h1>Index</h1>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data, 
+    },
+    revalidate: 60 * 60 * 8, 
+  }
 }
