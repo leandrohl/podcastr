c13d29550e1a9336f00a8e3568210ba18834c6a6
Leandro Henrique <leandro.henrique@unesp.br>
2021-04-21 15:00:55 -0300

    implementação do componente header

---
+++ src/pages/_document.tsx
@@ -0,0 +1,23 @@
+
+// substitui o index.html da aplicação react
+// o _document é executado somente uma vez
+
+
+import Document, {Html, Head, Main, NextScript} from 'next/document'
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link rel="preconnect" href="https://fonts.gstatic.com"/>
+                    <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet"/>
+                </Head>
+                <body>
+                    <Main/>
+                    <NextScript/>
+                </body>
+            </Html>
+        );
+    }
+}
