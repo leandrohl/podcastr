6a4e1ed484d944f26d366a40c790599224c051e2
Leandro Henrique <leandro.henrique@unesp.br>
2021-04-24 14:42:07 -0300

    finalizando o slug

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,8 +1,7 @@
 
-import {useRouter} from 'next/router'
 import {GetStaticPaths, GetStaticProps} from 'next'
 import { format, parseISO } from 'date-fns'
-
+import { useRouter} from 'next/router'
 import Link from 'next/link'
 import ptBR from 'date-fns/locale/pt-BR'
 
@@ -64,9 +63,26 @@ export default function Episode({episode} : EpisodeProps) {
     )
 }
 
+
 export const getStaticPaths: GetStaticPaths = async () => {
+    const {data} = await api.get('episodes', {
+        params: {
+            _limit: 2, 
+            _sort: 'published_at',
+            _order: 'desc'
+        }
+    })
+
+    const paths = data.map(episode => {
+        return {
+            params: {
+                slug: episode.id
+            }
+        }
+    })
+
     return {
-        paths: [],
+        paths,
         fallback: 'blocking'
     }
 }
